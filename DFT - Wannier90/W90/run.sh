db0acbba0ea46927ce711575dcd8e11a90d9eb39
tungduy1704 <daoduytung1704@gmail.com>
2025-05-31 10:31:50 +0700

    Update run.sh

--- DFT - Wannier90/W90/run.sh
+++ DFT - Wannier90/W90/run.sh
@@ -1,1 +1,10 @@
+mpirun -np 8 pw.x -in mos2.scf > scf.out
+mpirun -np 8 pw.x -in mos2.nscf > nscf.out
+wannier90.x -pp mos2
+mpirun -np 8 pw2wannier90.x -in mos2.pw2wan > pw2wan.out
+wannier90.x mos2
+
+# If you want to get the density of state, do the postw90
+postw90.x mos2
+
 
